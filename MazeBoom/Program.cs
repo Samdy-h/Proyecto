d7e9c36e18d9d4334dcc42d9bd3f4de1119c2191
Samdy-h <heidyvazquez439@gmail.com>
2025-01-06 13:59:12 -0500

    ok

--- MazeBoom/Program.cs
+++ MazeBoom/Program.cs
@@ -1,6 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using Spectre.Console;
+    enum Casilla
+    {
+        Walls,//representa un muro
+        trampa1,//representa un tipo de trampa
+        trampa2,//representa 
+        trampa3,
+        Free
+    }
+
 class Maze
 {
     static void Main()
@@ -10,14 +19,11 @@ class Maze
         Casilla[,] laberinto= GetMaze(ancho, altura);
         PrintMaze(laberinto);
     }
-    enum Casilla
-    {
-        Walls,
-        Free
-    }
     static  Casilla[,] GetMaze(int ancho, int altura)
     {
         Casilla[,] laberinto= new Casilla[ancho, altura];
+        Random rnd= new Random();
+
         for (int i=0;i<ancho;i++)
         {
             for(int j=0;j<altura;j++)
@@ -26,6 +32,11 @@ class Maze
             }
         }
         CrearCamnos(laberinto,1,1);
+        laberinto[0,1]=Casilla.Free;
+        laberinto[altura-1,ancho-2]=Casilla.Free;
+
+        AgregarTrampas(laberinto,3);
+
         return laberinto;
     }
     static void  CrearCamnos(Casilla[,] laberinto, int x, int y)
@@ -51,6 +62,37 @@ class Maze
             }
         }
     }
+    static void AgregarTrampas(Casilla[,] laberinto,int cantidadTrampas)
+    {
+        var rnd=new Random();
+        int filas=laberinto.GetLength(0);
+        int columnas=laberinto.GetLength(1);
+
+        for (int i=0;i<cantidadTrampas;i++)
+        {
+            while(true)
+            {
+                int x=rnd.Next(1,columnas-1);//evitar bordes
+                int y=rnd.Next(1,filas-1);//evitar bordes
+                if(laberinto[y,x]==Casilla.Free)//solo en casillas libres
+                {
+                    switch (i)
+                    {
+                        case 0:
+                        laberinto[y,x]=Casilla.trampa1;
+                        break;
+                        case 1:
+                        laberinto[y,x]=Casilla.trampa2;
+                        break;
+                        case 2:
+                        laberinto[y,x]=Casilla.trampa3;
+                        break;
+                    }
+                    break;//salir del bucle de colocar trampas
+                }
+            }
+        }
+    }
     static void PrintMaze(Casilla[,] laberinto)
     {
         int filas=laberinto.GetLength(0);
@@ -59,13 +101,23 @@ class Maze
         {
             for(int j=0;j<columnas;j++)
             {
-                if(laberinto[i,j]==Casilla.Walls)
+                switch (laberinto[i,j])
                 {
-                    AnsiConsole.Markup("[red]#[/]");
-                }
-                else
-                {
-                    AnsiConsole.Markup("[green][/]");
+                    case Casilla.Walls:
+                    AnsiConsole.Markup("[blue]&[/]");//paredes
+                    break;
+                    case Casilla.Free:
+                    AnsiConsole.Markup("[white]#[/]");//camino
+                    break;
+                    case Casilla.trampa1:
+                    AnsiConsole.Markup("[yellow]![/]");
+                    break;
+                    case Casilla.trampa2:
+                    AnsiConsole.Markup("[red]![/]");
+                    break;
+                    case Casilla.trampa3:
+                    AnsiConsole.Markup("[magenta]![/]");
+                    break;
                 }
             }
             Console.WriteLine();
